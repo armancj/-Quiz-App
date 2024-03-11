8e5a98644d25bb56fed442b0720e8a18d5747b14
mandi <armancanjer@gmail.com>
2024-03-11 12:37:29 -0400

    updated quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,29 +1,46 @@
 import {useState} from "react";
 import questions from "../questions.js";
+import quiz from '../assets/quiz-complete.png';
 
 export function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
 
-    function handleSelectAnswer(selectedAnswers){
-        setUserAnswers((prevState)=> {
+    const quizIsComplete = activeQuestionIndex === questions.length;
+
+    if(quizIsComplete){
+        return (
+            <div id={"summary"}>
+                <img src={quiz} alt={"quiz"}/>
+                <h2>Quiz Completed</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...questions[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=> Math.random() - 0.5);
+
+    function handleSelectAnswer(selectedAnswers) {
+        setUserAnswers((prevState) => {
             return [...prevState, selectedAnswers];
-        })
+        });
     }
 
     return (
-        <div id={"questions"}>
-            <h2>{questions[activeQuestionIndex].text}</h2>
-            <ul id={"answers"}>
-                {questions[activeQuestionIndex].answers.map((answer, index) => (
-                    <li key={index} className={"answer"}>
-                        <button onClick={()=>handleSelectAnswer(answer)}>
-                            {answer}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+        <div id={"quiz"}>
+            <div id={"questions"}>
+                <h2>{questions[activeQuestionIndex].text}</h2>
+                <ul id={"answers"}>
+                    {questions[activeQuestionIndex].answers.map((answer, index) => (
+                        <li key={`${answer}`} className={"answer"}>
+                            <button onClick={() => handleSelectAnswer(answer)} >
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
